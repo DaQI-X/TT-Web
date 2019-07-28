897f6b88a3cf3c0ffff6c32d3bf7755742b43cd3
Mr.Qi <123123@qk.com>
2019-07-29 03:29:21 +0800

    初始化

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+// 导入路由模块
 import router from './router'
+// 导入 vuex 模块
 import store from './store'
-
+// 导入 vantUI 
+import Vant from 'vant'
+// 引入 vant 样式
+import 'vant/lib/index.css'
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
--- src/store.js
+++ src/store.js
@@ -5,12 +5,23 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    // 声明数据
+    count: 10
   },
   mutations: {
-
+    // 设置数据
+    fn (state, pyload) {
+      pyload = 20
+      console.log('执行')
+      state.count = pyload
+    }
   },
   actions: {
-
+    // fn2 (context) {
+    //   setInterval(() => {
+    //     const NewCont = 20
+    //     context.commit('fn', NewCont)
+    //   }, 2000)
+    // }
   }
 })
